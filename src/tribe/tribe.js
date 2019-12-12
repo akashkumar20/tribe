a7014c13fdf5a668bc72347bd433cc91a25cfbc1
akashkumar20 <akash@iofactory.in>
2019-12-12 16:45:01 +0530

    tribe

---
+++ src/tribe/tribe.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import Avatar from './svg/avatar';
+
+class Tribe extends Component{
+    render(){
+        return(
+            <div>
+                <button>
+                    <Avatar />
+                    hi
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Tribe;
